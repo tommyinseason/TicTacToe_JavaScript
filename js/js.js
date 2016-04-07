41718523bbbf1ca637173ddff6b92863d9014485
Arlen Burton and Tommy Jones <arlen@>
2016-04-07 09:26:41 -0700

    accept input and push its value to an array.

--- js/js.js
+++ js/js.js
@@ -1,74 +1,50 @@
 //business logic stuff
+
 function Game(){
   this.player = "X";
-  this.space = [["","",""]["","",""]["","",""]];
+  this.space = [["","",""], ["","",""], ["","",""]];
 
   this.nextPlayer = function(){
     if(this.player === "X"){
       this.player = "O";
     } else { this.player = "X"; }
   }
-  this.setSpace = function(('div#1A'),('div#1B'),('div#1C'),('div#2A'),('div#2B'),('div#2C'),('div#3A'),('div#3B'),('div#3C')){
-
+  this.setSpace = function(row, col){
+    if(!this.space[row][col]){
+      this.space[row][col] = this.player;
+      return true;
+    } else { return false; }
   }
+
 }
 
 
 function Start() {
-  newGame = new Game();
+  var newGame = new Game();
   $('.boardSpace').text('');
 }
-
-
-var playerOneSelect = "X";
-var playerTwoSelect = "O";
-var clickCount = 0;
-
-
-
-
-
-
-
-
-// if((event.target).hasClass("O") || (event.target).hasClass("X")){
-//   $(this).text("");
-//   clickCount = clickCount - 1;
-//   console.log(clickCount);
-// }
-
 
 
 //user logic
 
 $(document).ready(function() {
 
+  var newGame = new Game();
+
   $('div.boardSpace').click(function() {
     $('h2.playerTurn').toggleClass('hidden');
-    clickCount = clickCount + 1;
-
 
    });
 
   $('div.boardSpace').click(function(event){
-    console.log(event.target);
 
     if (newGame.setSpace(parseInt($(this).parent().attr('id')), parseInt($(this).attr('value')))) {
+      $(this).text(newGame.player);
       $(event.target).addClass('active');
       newGame.nextPlayer();
-    }
-
-   if (clickCount%2 === 0){
-      $(event.target).text(playerTwoSelect);
-        //$(event.target).addClass("O");
-        console.log(clickCount);
-        console.log(event.target);
-    }
-   if (clickCount%2 !== 0){
-      $(event.target).text(playerOneSelect);
-        //$(event.target).addClass("X");
-        console.log(clickCount);
-        console.log(event.target);
+      console.log(newGame.space[0]);
+      console.log(newGame.space[1]);
+      console.log(newGame.space[2]);
     }
 
   });
